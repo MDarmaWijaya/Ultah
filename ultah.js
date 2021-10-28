ff3084afda8be6a0f4b5c63e2f9f95a17827427e
MDarmaWijaya <Wijayamdarma0@gmail.com>
2021-10-28 16:03:13 +0700

    Ultah

---
+++ ultah.js
@@ -0,0 +1,3 @@
+var siapa = prompt("Hai! masukan nama mu dulu");
+alert('hai '+siapa+' selamat ulang tahun ya semoga sehat selalu dan semoga tercapai apa yang di cita-citakan,Wish you all the best');
+alert("by darma")
